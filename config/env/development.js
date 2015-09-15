05f81491cda79eca6efd1a881b4fae302f8804d0
CJOYBLUV <dave_hemmer@hotmail.com>
2015-09-15 12:30:54 -0700

    setup for Heroku

--- config/env/development.js
+++ config/env/development.js
@@ -1,24 +1,16 @@
-/**
- * Development environment settings
- *
- * This file can include shared settings for a development team,
- * such as API keys or remote database passwords.  If you're using
- * a version control solution for your Sails app, this file will
- * be committed to your repository unless you add it to your .gitignore
- * file.  If your repository will be publicly viewable, don't add
- * any private information to this file!
- *
- */
-
 module.exports = {
 
-  /***************************************************************************
-   * Set the default database connection for models in the development       *
-   * environment (see config/connections.js and config/models.js )           *
-   ***************************************************************************/
-
-  // models: {
-  //   connection: 'someMongodbServer'
-  // }
-
+    connections:{
+        thisMongoDbServer: {
+            adapter: 'sails-mongo',
+            host: 'localhost',
+            port: 27017,
+            database: 'wondercampers_dev'
+        }
+    },
+    models:{
+        connection: 'thisMongoDbServer',
+        migrate: 'alter'
+    }
 };
+
